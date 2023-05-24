f72154357e87aef2f7d7ce45691d0b7736213c23
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2023-05-24 11:35:14 +0300

    refactor: change text for home page and margin

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,13 +6,13 @@ function Home(props) {
   return (
     <>
       <main className='main'>
-        <h2 className='pages-title'>Here you will find the best restaurants</h2>
+        <h2 className='pages-title'>Here you will save the best restaurants</h2>
         <div className='home-wrapper'>
           <div className='home-items'>
             <img className='home-image' src={defultImage} alt="Chef" />
           </div>
           <div className='home-items'>
-            <p className='home-text'>Follow the link and enjoy watching!</p>
+            <p className='home-text'>You can create, edit, and delete restaurant cards. Follow the link and see the list of restaurants. </p>
             <Link to={`/restaurants`}>
               <button className="btn waves-effect waves-light form-check-btn">
                 Look restaurants
